03731b0ab80afd12fbbb3b93ba31e599cab7c7ba
Teis Lindemark <teis.lindemark@gmail.com>
2015-08-27 20:04:20 +0200

    First commit

---
+++ Scripts/Package.ps1
@@ -0,0 +1,5 @@
+﻿$makeAppxpath = "C:\Program Files (x86)\Windows Kits\8.0\bin\x64\makeappx.exe";
+$inputDirectory = ".\TestPackageingApp";
+
+$exe = $makeAppxpath
+&$exe pack /v /d $inputDirectory /p TestPackageingApp.appx
