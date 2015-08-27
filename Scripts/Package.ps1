5cd4a08e71c40f693b5e4c3bdf65db81a315d1aa
Teis Lindemark <teis.lindemark@gmail.com>
2015-08-27 21:42:41 +0200

    Fikser

--- Scripts/Package.ps1
+++ Scripts/Package.ps1
@@ -1,19 +1,32 @@
-﻿$makeAppxpath = "C:\Program Files (x86)\Windows Kits\8.0\bin\x64\makeappx.exe";
+﻿# ============================================================
+# Package script Windows Store Applications
+# ------------------------------------------------------------
+# Created by Teis Lindemark
+# Date 23.08.2015
+# ============================================================
+
+$basePath = "C:\Program Files (x86)\Windows Kits\8.0\bin\x64\";
 $inputDirectory = ".\TestPackageingApp";
+$outputPackage = ".\Packeage\TestPackageingApp.appx";
 
 Write-Host "Pakker app..."
-$exe = $makeAppxpath
-&$exe pack /v /d $inputDirectory /p .\Packeage\TestPackageingApp.appx
+$exe = $basePath + "makeappx.exe"
+&$exe pack /v /o /d $inputDirectory /p .\Packeage\TestPackageingApp.appx
 Write-Host "Pakking ferdig."
+
+Write-Host "Genererer sertifikat"
+#$exe = $basePath + "makecert"
+#&$exe -a sha1 -sk "peterdn.com" -iv teilin.net.pvk -n "CN=teilin.net" -ic teilin.net.cer -sr currentuser -ss My
+Write-Host "Ferdig å generere sertifikat"
 
 Write-Host "Signerer app pakke..."
 $certThumbprint = Get-ChildItem -path cert:\LocalMachine\My | Where-Object {$_.Subject -match "CN=teis"}
-$exe = "C:\Program Files (x86)\Windows Kits\8.0\bin\x64\signtool.exe"
-&$exe sign /fd sha256 /sha1 $certThumbprint.Thumbprint .\Packeage\TestPackageingApp.appx
+$exe = $basePath + "signtool.exe"
+&$exe sign /fd sha256 /sha1 $certThumbprint.Thumbprint $outputPackage
 Write-Host "Signering ferdig."
 
 Write-Host "Installing..."
-Add-AppxPackage .\Packeage\TestPackageingApp.appx
+#Add-AppxPackage .\Packeage\TestPackageingApp.appx
 Write-Host "Ferdig med installering."
 
 # Deploy
